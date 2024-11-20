ba891fa785d47255fbbaf333fd8be8e4e55b3480
Ni <daisytsai11336@gmail.com>
2024-11-20 11:04:07 +0800

    chore: clean up useless file and manage the structure for this project

---
+++ src/common/navigationBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NavigationBar(): JSX.Element {
+    return (
+        <div className="w-dvh flex justify-between">
+            <div>nav</div>
+            <div></div>
+        </div>
+    )
+}
